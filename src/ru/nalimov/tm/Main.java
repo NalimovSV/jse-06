e595a90f96c05df24427bd03b9219fbc221ff5a9
Sergey Nalimov <nalimov_sv@nlmk.com>
2020-07-09 23:34:16 +0500

    сделан терминал рефакторинг

--- src/ru/nalimov/tm/Main.java
+++ src/ru/nalimov/tm/Main.java
@@ -1,11 +1,11 @@
 package ru.nalimov.tm;
 
+import static ru.nalimov.tm.constant.TerminalConst.*;
 
-import java.util.Arrays;
+/*Тестовое*/
 
 public class Main {
     public static void main(final String[] args) {
-       System.out.println(Arrays.toString(args));
        run(args);
        displayWelcome();
     }
@@ -14,17 +14,40 @@ public class Main {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
-        if ("ver".equals(param)) displayVer();
+        switch (param) {
+            case VERSION: displayVer();
+            case ABOUT: displayAbout();
+            case HELP: displayHelp();
+            default: displayErr();
+        }
 
     }
     private  static void displayVer() {
-        System.out.println("ver1");
+        System.out.println("ver 1.0");
+        System.exit(0);
+    }
+
+    private  static void displayAbout() {
+        System.out.println("nalimov_sv@nlmk.com");
+        System.exit(0);
+    }
+
+    private  static void displayHelp() {
+        System.out.println("version - Display program version.");
+        System.out.println("about - Display developer info.");
+        System.out.println("help - Display list of terminal commands.");
         System.exit(0);
     }
 
     private  static void displayWelcome() {
         System.out.println("Welcome");
     }
+
+    private  static void displayErr() {
+        System.out.println("Error! Unknow key...");
+        System.exit(-1);
+    }
+
 }
 
 
---
+++ src/ru/nalimov/tm/constant/TerminalConst.java
@@ -0,0 +1,7 @@
+package ru.nalimov.tm.constant;
+
+public class TerminalConst {
+    public static final String HELP = "help";
+    public static final String VERSION = "ver";
+    public static final String ABOUT = "about";
+}
