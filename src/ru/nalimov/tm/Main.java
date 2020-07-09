f366f0977903af4dd033ba2ed08635ab60a15974
Sergey Nalimov <nalimov_sv@nlmk.com>
2020-07-09 22:42:10 +0500

    сделан терминал

--- src/ru/nalimov/tm/Main.java
+++ src/ru/nalimov/tm/Main.java
@@ -1,9 +1,30 @@
 package ru.nalimov.tm;
 
-public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("HELLO WORLD");
+import java.util.Arrays;
+
+public class Main {
+    public static void main(final String[] args) {
+       System.out.println(Arrays.toString(args));
+       run(args);
+       displayWelcome();
     }
 
+    private static void run(final String[] args) {
+        if (args == null) return;
+        if (args.length < 1) return;
+        final String param = args[0];
+        if ("ver".equals(param)) displayVer();
+
+    }
+    private  static void displayVer() {
+        System.out.println("ver1");
+        System.exit(0);
+    }
+
+    private  static void displayWelcome() {
+        System.out.println("Welcome");
+    }
 }
+
+
