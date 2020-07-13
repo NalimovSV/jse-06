2834b34a76573419a0fd445101eb837506db172f
Sergey Nalimov <nalimov_sv@nlmk.com>
2020-07-13 14:59:59 +0000

    Update app.java

--- src/main/java/ru/nalimov/tm/app.java
+++ src/main/java/ru/nalimov/tm/app.java
@@ -1,55 +1,9 @@
 package ru.nalimov.tm;
 
-import static ru.nalimov.tm.constant.TerminalConst.*;
+public class App {
 
-/*Тестовое*/
-
-public class app {
-
-    public static void main(final String[] args) {
-       run(args);
-       displayWelcome();
-    }
-
-    private static void run(final String[] args) {
-        if (args == null) return;
-        if (args.length < 1) return;
-        final String param = args[0];
-        switch (param) {
-            case VERSION: displayVersion();
-            case ABOUT: displayAbout();
-            case HELP: displayHelp();
-            default: displayErr();
-        }
-
-    }
-
-    private  static void displayVersion() {
-        System.out.println("ver 1.0");
-        System.exit(0);
-    }
-
-    private  static void displayAbout() {
-        System.out.println("nalimov_sv@nlmk.com");
-        System.exit(0);
-    }
-
-    private  static void displayHelp() {
-        System.out.println("version - Display program version.");
-        System.out.println("about - Display developer info.");
-        System.out.println("help - Display list of terminal commands.");
-        System.exit(0);
-    }
-
-    private  static void displayWelcome() {
-        System.out.println("Welcome");
-    }
-
-    private  static void displayErr() {
-        System.out.println("Error! Unknow key...");
-        System.exit(-1);
+    public static void main(String[] args) {
+	System.out.println("HELLO WORLD");
     }
 
 }
-
-
