14daa414ade1677516e4b64b1be61391a9b7aa7e
Sergey Nalimov <nalimov_sv@nlmk.com>
2020-07-14 18:24:43 +0000

    Delete TerminalConst.java

--- src/main/java/ru/nalimov/tm/constant/TerminalConst.java
+++
@@ -1,7 +0,0 @@
-package ru.nalimov.tm.constant;
-
-public class TerminalConst {
-    public static final String HELP = "help";
-    public static final String VERSION = "version";
-    public static final String ABOUT = "about";
-}
